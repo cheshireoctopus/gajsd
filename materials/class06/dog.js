d406522d2f29c112b57e9e8e6e73d36fc4fec39b
Chandler Moisen <chandlermoisen@gmail.com>
2018-02-25 18:31:56 -0500

    class 06/07 switch

---
+++ materials/class06/dog.js
@@ -0,0 +1,19 @@
+/*
+
+Work with a partner to create a Dog constructor, which creates instances containing the following properties:
+
+* name (string)
+* species (string)
+* foodsEaten (array)
+
+And the following methods:
+
+* eatSomething(food): takes the 'food' argument (passed as a string) and pushes it into the 'foodsEaten' array
+* introduce: console.logs a string introducing itself, including its name, species, and what it has eaten.
+
+Create 3 dogs total. Make sure all 3 dogs have all properties set and methods defined.
+
+Exercise your dogs by retrieving their properties and using their methods. Practice using both syntaxes
+for retrieving properties (dot notation and brackets).
+
+*/
