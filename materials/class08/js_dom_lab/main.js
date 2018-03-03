275612743ea7a144dc00f10dfaa0c9eccaa39952
Chandler Moisen <chandlermoisen@gmail.com>
2018-03-03 16:38:30 -0500

    update class08

---
+++ materials/class08/js_dom_lab/main.js
@@ -0,0 +1,17 @@
+/* Making a favorites list: DOM manipulation
+
+- When the user clicks the submit button, take the value they've typed
+  into the input box and add it to the list (hint: appendChild)
+
+- Also, when a new item is added to the list, clear the input box.
+
+Bonus:
+
+When the user clicks the button without typing anything, alert the user with alert('your message here')
+Hint: the `value` property of an empty input box is an empty string: ""
+
+*/
+
+
+
+
