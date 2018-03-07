ef0f358b9528e74bff041cd7d8d1808604a31dac
Chandler Moisen <chandlermoisen@gmail.com>
2018-03-07 12:44:37 -0500

    update class09

---
+++ materials/class09/templates_intro/app.js
@@ -0,0 +1,42 @@
+var ourClass = [
+    {
+        name: 'Chandler',
+        role: 'Instructor',
+    },
+    {
+        name: 'Matt',
+        role: 'Student',
+    },
+    {
+        name: 'Rachel',
+        role: 'Student',
+    },
+    {
+        name: 'Ari',
+        role: 'Student',
+    },
+    {
+        name: 'Rish',
+        role: 'Student',
+    },
+    {
+        name: 'Jessie',
+        role: 'Student',
+    },
+    {
+        name: 'Yogi',
+        role: 'Student',
+    },
+    {
+        name: 'Lewis',
+        role: 'Student',
+    },
+    {
+        name: 'Sino',
+        role: 'Student',
+    },
+    {
+        name: 'Desmond',
+        role: 'Student',
+    },
+]
